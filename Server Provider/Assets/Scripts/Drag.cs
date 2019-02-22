1697aae443d30341c9b28f0d032ce8ce793d74b1
Melih <mlhtncc@gmail.com>
2019-02-23 01:06:25 +0300

    Added IDragHandler, it is not done, it is a start :beer:

---
+++ Server Provider/Assets/Scripts/Drag.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Drag : MonoBehaviour, IDragHandler, IPointerDownHandler, IPointerUpHandler
+{
+    private GameObject draggingOriginal;
+    private GameObject draggingClone;
+
+    GraphicRaycaster m_Raycaster;
+    PointerEventData m_PointerEventData;
+    EventSystem m_EventSystem;
+
+    void Start()
+    {
+        //Fetch the Raycaster from the GameObject (the Canvas)
+        m_Raycaster = GetComponentInParent<GraphicRaycaster>();
+        //Fetch the Event System from the Scene
+        m_EventSystem = EventSystem.current;
+    }
+
+    //Detect current clicks on the GameObject (the one with the script attached)
+    public void OnPointerDown(PointerEventData pointerEventData)
+    {
+        //Set up the new Pointer Event
+        m_PointerEventData = new PointerEventData(m_EventSystem);
+        //Set the Pointer Event Position to that of the mouse position
+        m_PointerEventData.position = Input.mousePosition;
+
+        //Create a list of Raycast Results
+        List<RaycastResult> results = new List<RaycastResult>();
+
+        //Raycast using the Graphics Raycaster and mouse click position
+        m_Raycaster.Raycast(m_PointerEventData, results);
+
+        //For every result returned, output the name of the GameObject on the Canvas hit by the Ray
+        string res = "";
+        foreach (RaycastResult result in results)
+        {
+            res += "Hit " + result.gameObject.name + "  ";
+        }
+        Debug.Log(res);
+
+        draggingOriginal = results[0].gameObject;
+
+
+        var canvas = FindInParents<Canvas>(gameObject);
+        if (canvas == null)
+            return;
+
+        // We have clicked something that can be dragged.
+        // What we want to do is create an icon for this.
+        draggingClone = Instantiate(draggingOriginal);
+
+        draggingClone.transform.SetParent(canvas.transform, false);
+        draggingClone.transform.SetAsLastSibling();
+
+        SetDraggedPosition(pointerEventData);
+    }
+
+
+    public void OnDrag(PointerEventData data)
+    {
+        if (draggingClone != null)
+            SetDraggedPosition(data);
+    }
+
+    private void SetDraggedPosition(PointerEventData data)
+    {
+        var rt = draggingClone.GetComponent<RectTransform>();
+        Vector3 globalMousePos;
+        if (RectTransformUtility.ScreenPointToWorldPointInRectangle(draggingClone.transform as RectTransform,
+                data.position, data.pressEventCamera, out globalMousePos))
+        {
+            rt.position = globalMousePos;
+            rt.rotation = draggingClone.transform.rotation;
+        }
+    }
+
+    public void OnPointerUp(PointerEventData pointerEventData)
+    {
+        if (draggingClone != null)
+            Destroy(draggingClone);
+    }
+
+    static public T FindInParents<T>(GameObject go) where T : Component
+    {
+        if (go == null) return null;
+        var comp = go.GetComponent<T>();
+
+        if (comp != null)
+            return comp;
+
+        Transform t = go.transform.parent;
+        while (t != null && comp == null)
+        {
+            comp = t.gameObject.GetComponent<T>();
+            t = t.parent;
+        }
+        return comp;
+    }
+}
+
